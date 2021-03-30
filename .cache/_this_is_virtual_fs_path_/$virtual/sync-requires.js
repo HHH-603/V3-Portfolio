73af4c44591fa294c5ee22d2fd46fe4c613e713c
HHH-603 <hholmes726@gmail.com>
2021-03-30 13:22:00 -0400

    Used Gatsby to create starter files/folder directory for my portfolio.

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/home/hhh603/Documents/Projects/V3-Portfolio/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/home/hhh603/Documents/Projects/V3-Portfolio/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/home/hhh603/Documents/Projects/V3-Portfolio/src/pages/index.js"))
+}
+
