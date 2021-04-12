40133446def29dd9c038c9a8f7f45174dcf6cfce
HHH-603 <hholmes726@gmail.com>
2021-04-12 12:42:37 -0400

    Removed hover effect from my footer component's links.

--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -4,7 +4,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-caches-gatsby-plugin-offline-app-shell-js": preferDefault(require("/home/hhh-603/Documents/Projects/V3-Portfolio_Gatsby/.cache/caches/gatsby-plugin-offline/app-shell.js")),
+  "component---cache-dev-404-page-js": preferDefault(require("/home/hhh-603/Documents/Projects/V3-Portfolio_Gatsby/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/home/hhh-603/Documents/Projects/V3-Portfolio_Gatsby/src/pages/404.js")),
   "component---src-pages-about-js": preferDefault(require("/home/hhh-603/Documents/Projects/V3-Portfolio_Gatsby/src/pages/about.js")),
   "component---src-pages-contact-js": preferDefault(require("/home/hhh-603/Documents/Projects/V3-Portfolio_Gatsby/src/pages/contact.js")),
