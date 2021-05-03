2f9a8f822a393730e08af04b38d29d6452c2ef3e
HHH-603 <hholmes726@gmail.com>
2021-05-03 11:14:05 -0400

    Edited footer, layout, and container stying so that the footer will always remain at the bottom of the page AND body; also added the FAST_DEV flag to the siteMetaData.

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,9 @@ module.exports = {
     siteUrl: "https://hankholmes.me",
     image: "https://i.imgur.com/smcSJLz.jpg",
     twitterUsername: "@Sherlock774",
+    flags: {
+      FAST_DEV: true,
+    },
   },
   plugins: [
     "gatsby-plugin-sass",
