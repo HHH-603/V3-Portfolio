fdd9a86efbe2eae0f1ccf33702282fe755c684f5
HHH-603 <hholmes726@gmail.com>
2021-05-13 14:39:02 -0400

    Deleted the static/image folder(s) and updated siteMetadata url with new pathway to see if that fixes my meta og:image problem.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
       "Hank is a Front End Web Developer based in Charlotte, NC who enjoyes designing + building websites, web applications, and unified software solutions.",
     author: "Hank Holmes",
     siteUrl: "https://hankholmes.me",
-    image: "/images/alaska.webp",
+    image: "/images/alaska.jpg",
     twitterUsername: "@Sherlock774",
   },
   plugins: [
